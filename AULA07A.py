44ff1b9441b3ce6a773102eec6045b5e0101cdc8
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 12:27:35 -0300

    Add files via upload

---
+++ AULA07A.py
@@ -0,0 +1,7 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor:'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+print('A soma dos valores é {},a multiplicação é {} e a divisão é {}'.format(s, m, d), end='')
+
